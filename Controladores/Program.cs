687226402639b755de9de901bbe1969a29a1652c
DESKTOP-ALP9J2M\Usuario <csi23-mserina@colegioaltair.net>
2023-10-24 01:05:16 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,30 @@
+﻿using numeroCuadrado.Servicios;
+
+namespace numeroCuadrado
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+
+            NumeroInterfaz nu = new NumeroImplementacion();
+            string numeroAlmacenado;
+            numeroAlmacenado = nu.solicitudNumero();
+            bool esEntero;
+            
+            esEntero= double.TryParse(numeroAlmacenado, out double numeroAftherConversion);
+
+            if (esEntero)
+            {
+                double raizCuadrada;
+                raizCuadrada = Math.Sqrt(numeroAftherConversion);
+                Console.WriteLine("La raiz cuadrada de " + numeroAftherConversion + "es " + raizCuadrada); 
+            }
+            else
+            {
+                Console.WriteLine("No se puede calcular la raiz cuadrada con numeros negativos");
+            }
+            
+        }
+    }
+}
---
+++ Servicios/NumeroImplementacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeroCuadrado.Servicios
+{
+    internal class NumeroImplementacion : NumeroInterfaz
+    {
+        public string solicitudNumero()
+        {
+            string numero;
+            Console.WriteLine("Dame un numero");
+            numero = Console.ReadLine();
+            return numero;
+
+        }
+
+        
+    }
+}
